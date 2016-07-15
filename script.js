30a6b674f9794ca0638341fe64704aa8922cb8c0
benhassara <bhassara@gmail.com>
2016-07-15 16:37:34 -0600

    initial boilerplate

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    // append an img tag with our picture of Ryu
+    // url: http://i.imgur.com/90Mmdcm.png
+
+    // change the img src on hover to the animated gif of Ryu
+    // url: http://i.imgur.com/nTj3Fxx.gif
+    // when the user 'unhovers' change back to static Ryu
+
+    // when a user clicks, change Ryu's stance
+    // url: http://i.imgur.com/Rfj0a80.png
+    
+});
