dcf4121814f0a36e6420f04570505114f5e7d0e2
benhassara <bhassara@gmail.com>
2016-07-17 10:21:36 -0600

    add Ryu hover animation

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ $(document).ready(function() {
     // change the img src on hover to the animated gif of Ryu
     // url: http://i.imgur.com/nTj3Fxx.gif
     // when the user 'unhovers' change back to static Ryu
+    $('#ryu > img').hover(function() {
+        this.src = 'http://i.imgur.com/nTj3Fxx.gif';
+    }, function() {
+        this.src = 'http://i.imgur.com/90Mmdcm.png';
+    });
 
     // when a user clicks, change Ryu's stance
     // url: http://i.imgur.com/Rfj0a80.png
